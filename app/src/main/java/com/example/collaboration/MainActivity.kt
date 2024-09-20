856bfc0eb412c0e4db6f00e5754271a32224da65
collaboration <sambalidojaira@gmail.com>
2024-09-20 10:59:06 +0800

    Second Commit!!!!!!

--- app/src/main/java/com/example/collaboration/MainActivity.kt
+++ app/src/main/java/com/example/collaboration/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             COLLABORATIONTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Collab",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
